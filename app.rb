a91763d85b1b9c9221a0b0a3fa74310d84acdd82
mitchthorson <mthorson@usatoday.com>
2015-02-14 19:24:20 -0500

    add get request handler that adds object and returns full array of objects

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 require 'json'
 
-data = {
+data = [{
   :name => 'test1',
   :is_done => 'false'
-}
+}]
 
 get '/' do
+  content_type :json
+  data.to_json
+end
+
+post '/' do
+  new = JSON.parse(request.body.read)
+  data << new
   content_type :json
   data.to_json
 end
